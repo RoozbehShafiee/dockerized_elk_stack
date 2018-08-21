fdb3b1da3d6a48e45a91a1c039a76c6f3fe5fb12
Roozbeh Shafiee <r.shafiee@celonis.de>
2018-08-21 16:43:58 +0200

    almost clean and done

--- kibana/conf_files/entrypoint.sh
+++ kibana/conf_files/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -m
+set -e
+
+HOST=${HOST:-}
+PORT=${PORT:-}
+ES_URL=${ES_HOST:-}
+
 function config_kibana {
-    set -e
-    HOST=${HOST:-}
-    PORT=${PORT:-}
-    ES_URL=${ES_URL:-}
     sed -i "s|#server.host: \"localhost\"|server.host: \"${HOST}\"|g" /etc/kibana/kibana.yml
     sed -i "s|#server.port: 5601|server.port: ${PORT}|g" /etc/kibana/kibana.yml
-    sed -i "s|#elasticsearch.url: \"http://localhost:9200\"|elasticsearch.url: \"http://${ES_URL}:9200\"|g" /etc/kibana/kibana.yml
+    sed -i "s|#elasticsearch.url: \"http://localhost:9200\"|elasticsearch.url: \"http://${ES_HOST}:9200\"|g" /etc/kibana/kibana.yml
 }
 
 function run_kibana {
--- logstash/conf_files/entrypoint.sh
+++ logstash/conf_files/entrypoint.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+
+set -m
 set -e
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- logstash "$@"
-fi
+ES_HOST=${ES_HOST:-}
+ES_PORT=${ES_PORT:-}
+SUPPORT_SSL=${SUPPORT_SSL:-}
 
-# Run as user "logstash" if the command is "logstash"
-# allow the container to be started with `--user`
-if [ "$1" = 'logstash' -a "$(id -u)" = '0' ]; then
-	set -- gosu logstash "$@"
-fi
+function support_ssl {
+  if [ $SUPPORT_SSL = "yes" ]; then
+      sed -i "s|SUPPORT_SSL|true|g" /etc/logstash/conf.d/filebeat-input.conf
+  else
+      sed -i "s|SUPPORT_SSL|false|g" /etc/logstash/conf.d/filebeat-input.conf
+      sed -i '/ssl_certificate/d' /etc/logstash/conf.d/filebeat-input.conf
+      sed -i '/ssl_key/d' /etc/logstash/conf.d/filebeat-input.conf
+  fi
+}
 
-exec "$@"
+function config_logstash {
+    sed -i "s|ES_HOST|${ES_HOST}|g" /etc/logstash/conf.d/output-elasticsearch.conf
+		sed -i "s|ES_PORT|${ES_PORT}|g" /etc/logstash/conf.d/output-elasticsearch.conf
+}
+
+function run_logstash {
+    /usr/share/logstash/bin/logstash
+}
+
+support_ssl
+config_logstash
+run_logstash
