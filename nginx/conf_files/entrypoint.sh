7534e14cf7a840d8daa06eb05b324f1d5d51c547
Roozbeh Shafiee <r.shafiee@celonis.de>
2018-08-22 16:56:16 +0200

    removing non-ssl deployment option

--- nginx/conf_files/entrypoint.sh
+++ nginx/conf_files/entrypoint.sh
@@ -3,10 +3,8 @@
 set -m
 set -e
 
-KIBANA_FQDN=${KIBANA_FQDN:-}
 KIBANA_URL=${KIBANA_URL:-}
 KIBANA_PORT=${KIBANA_PORT:-}
-LOGSTASH_FQDN=${LOGSTASH_FQDN:-}
 LOGSTASH_URL=${LOGSTASH_URL:-}
 LOGSTASH_PORT=${LOGSTASH_PORT:-}
 ACCESS_USER=${ACCESS_USER:-}
@@ -16,18 +14,14 @@ function set_credentials {
     echo "${ACCESS_USER}:`openssl passwd -apr1 ${ACCESS_PASSWORD}`" | tee -a /etc/nginx/htpasswd.kibana
 }
 
-function set_kibana {
-      sed -i "s|FQDN|${KIBANA_FQDN}|g" /etc/nginx/sites-available/kibana
+function vhost_kibana {
       sed -i "s|kibana-url:kibana-port|${KIBANA_URL}:${KIBANA_PORT}|g" /etc/nginx/sites-available/kibana
       ln -sf /etc/nginx/sites-available/kibana /etc/nginx/sites-enabled/kibana
-      chmod 600 /etc/nginx/ssl/kibana/*
 }
 
-function set_logstash {
-      sed -i "s|FQDN|${LOGSTASH_FQDN}|g" /etc/nginx/sites-available/logstash
+function vhost_logstash {
       sed -i "s|logstash-url:logstash-port|${LOGSTASH_URL}:${LOGSTASH_PORT}|g" /etc/nginx/sites-available/logstash
       ln -sf /etc/nginx/sites-available/logstash /etc/nginx/sites-enabled/logstash
-      chmod 600 /etc/nginx/ssl/logstash/*
 }
 
 function run_nginx {
@@ -35,6 +29,6 @@ function run_nginx {
 }
 
 set_credentials
-set_kibana
-set_logstash
+vhost_kibana
+vhost_logstash
 run_nginx
