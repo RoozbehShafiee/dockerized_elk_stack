fe3130aaf807720d1fe025d5e387ded555047279
Roozbeh Shafiee <roozbeh.shafiee@gmail.com>
2018-08-22 02:19:10 +0200

    nginx as logstash reverse proxy - removing non-ssl deployment

--- logstash/conf_files/entrypoint.sh
+++ logstash/conf_files/entrypoint.sh
@@ -5,17 +5,6 @@ set -e
 
 ES_HOST=${ES_HOST:-}
 ES_PORT=${ES_PORT:-}
-SUPPORT_SSL=${SUPPORT_SSL:-}
-
-function support_ssl {
-  if [ $SUPPORT_SSL = "yes" ]; then
-      sed -i "s|SUPPORT_SSL|true|g" /etc/logstash/conf.d/filebeat-input.conf
-  else
-      sed -i "s|SUPPORT_SSL|false|g" /etc/logstash/conf.d/filebeat-input.conf
-      sed -i '/ssl_certificate/d' /etc/logstash/conf.d/filebeat-input.conf
-      sed -i '/ssl_key/d' /etc/logstash/conf.d/filebeat-input.conf
-  fi
-}
 
 function config_logstash {
     sed -i "s|ES_HOST|${ES_HOST}|g" /etc/logstash/conf.d/output-elasticsearch.conf
@@ -26,6 +15,5 @@ function run_logstash {
     /usr/share/logstash/bin/logstash
 }
 
-support_ssl
 config_logstash
 run_logstash
--- nginx/conf_files/entrypoint.sh
+++ nginx/conf_files/entrypoint.sh
@@ -3,10 +3,12 @@
 set -m
 set -e
 
-FQDN=${FQDN:-}
-SUPPORT_SSL=${SUPPORT_SSL:-}
+KIBANA_FQDN=${KIBANA_FQDN:-}
 KIBANA_URL=${KIBANA_URL:-}
 KIBANA_PORT=${KIBANA_PORT:-}
+LOGSTASH_FQDN=${LOGSTASH_FQDN:-}
+LOGSTASH_URL=${LOGSTASH_URL:-}
+LOGSTASH_PORT=${LOGSTASH_PORT:-}
 ACCESS_USER=${ACCESS_USER:-}
 ACCESS_PASSWORD=${ACCESS_PASSWORD:-}
 
@@ -14,17 +16,18 @@ function set_credentials {
     echo "${ACCESS_USER}:`openssl passwd -apr1 ${ACCESS_PASSWORD}`" | tee -a /etc/nginx/htpasswd.kibana
 }
 
-function support_ssl {
-  if [ $SUPPORT_SSL = "yes" ]; then
-      sed -i "s|FQDN|${FQDN}|g" /etc/nginx/sites-available/443-default
-      sed -i "s|kibana-url:kibana-port|${KIBANA_URL}:${KIBANA_PORT}|g" /etc/nginx/sites-available/443-default
-      ln -sf /etc/nginx/sites-available/443-default /etc/nginx/sites-enabled/443-default
-      chmod 600 /etc/nginx/ssl/*
-  else
-      sed -i "s|FQDN|${FQDN}|g" /etc/nginx/sites-available/80-default
-      sed -i "s|kibana-url:kibana-port|${KIBANA_URL}:${KIBANA_PORT}|g" /etc/nginx/sites-available/80-default
-      ln -sf /etc/nginx/sites-available/80-default /etc/nginx/sites-enabled/80-default
-  fi
+function set_kibana {
+      sed -i "s|FQDN|${KIBANA_FQDN}|g" /etc/nginx/sites-available/kibana
+      sed -i "s|kibana-url:kibana-port|${KIBANA_URL}:${KIBANA_PORT}|g" /etc/nginx/sites-available/kibana
+      ln -sf /etc/nginx/sites-available/kibana /etc/nginx/sites-enabled/kibana
+      chmod 600 /etc/nginx/ssl/kibana/*
+}
+
+function set_logstash {
+      sed -i "s|FQDN|${LOGSTASH_FQDN}|g" /etc/nginx/sites-available/logstash
+      sed -i "s|logstash-url:logstash-port|${LOGSTASH_URL}:${LOGSTASH_PORT}|g" /etc/nginx/sites-available/logstash
+      ln -sf /etc/nginx/sites-available/logstash /etc/nginx/sites-enabled/logstash
+      chmod 600 /etc/nginx/ssl/logstash/*
 }
 
 function run_nginx {
@@ -32,5 +35,6 @@ function run_nginx {
 }
 
 set_credentials
-support_ssl
+set_kibana
+set_logstash
 run_nginx
